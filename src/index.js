9a1093377bce60048ed02172818f5cd81d89c883
Ihor Kopach <kopach@users.noreply.github.com>
2020-01-05 19:59:32 +0100

    Add version command

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import pkg from '../package.json';
-
 const program = require("commander");
-program.version(pkg.version, '-v, --version');
-
-program
-  .option("-d, --debug", "output extra debugging")
-  .option("-s, --small", "small pizza size")
-  .option("-c, --cheese <type>", "add the specified type of cheese", "blue")
-  .option("-p, --pizza-type <type>", "flavour of pizza");
+program.version(require("../package.json").version, "-v, --version");
 
 program.parse(process.argv);
-
-if (program.debug) console.log(program.opts());
-console.log("pizza details:");
-if (program.small) console.log("- small pizza size");
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
-console.log(`cheese: ${program.cheese}`);
